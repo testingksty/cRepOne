3ef2809c393a80e208cd735d1418574c7074719e
vas070707 <vas070707@gmail.com>
2024-12-13 23:36:34 +0000

    successfully approved

---
+++ src/test/java/priyankadecfinal/samplefirstprogram.java
@@ -0,0 +1,16 @@
+package priyankadecfinal;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class samplefirstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("all the best for your job trails");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+
+	}
+
+}
